4b0da825bebefa5f4dd52d51ae4cffe2a88632d7
Manuelcaballeroperez <95278769+Manuelaballeroperez@users.noreply.github.com>
2022-07-15 18:28:07 +0200

    Add files via upload

---
+++ Tema1.java
@@ -0,0 +1,11 @@
+package tema1;
+
+public class Tema1 {
+    public static void main(String[] args) {
+        int numero = 1;
+        String frase = "frase";
+        boolean condicion = true;
+        long numeroLong = 123456789;
+        double precio = 15.8;
+    }
+}
