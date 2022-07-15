91ae0a2679419a4eb641f265ff29e4595d4df6bd
Manuelcaballeroperez <95278769+Manuelaballeroperez@users.noreply.github.com>
2022-07-15 19:08:01 +0200

    Add files via upload

---
+++ Tema3.java
@@ -0,0 +1,16 @@
+package tema1;
+
+public class Tema3 {
+
+
+
+    public static void main(String[] args) {
+
+        double resultado = suma(10);
+    System.out.println(resultado);
+    }
+
+static double suma (double precio) {
+        return precio * 1.21;
+     }
+}
