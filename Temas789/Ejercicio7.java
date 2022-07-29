a7daae01e71120b275902e51cf836c8b44869f8f
Manuelcaballeroperez <95278769+Manuelaballeroperez@users.noreply.github.com>
2022-07-29 16:03:40 +0200

    Add files via upload

--- Temas789/Ejercicio7.java
+++ Temas789/Ejercicio7.java
@@ -1,12 +1,31 @@
 package Temas789;
 
+import java.util.Scanner;
+
 public class Ejercicio7 {
+    public static class dividePorCero {
+
+        private static int dividir(int a, int b) throws ArithmeticException {
+
+
+            return a / b;
+        }
+    }
+
 
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Introducieros dos numeros para dividir: ");
+
+        System.out.println("Primer numero: ");
+        int n = scanner.nextInt();
+        System.out.println("Segundo numero: ");
+        int m = scanner.nextInt();
+
+        int resultado = dividePorCero.dividir(n,m);
+        System.out.println("Resultado: " + resultado);
     }
 
-     public void dividePorCero () throws ArithmeticException {
-        System.out.println("Esto no puede hacerse");
-     }
+
 }
